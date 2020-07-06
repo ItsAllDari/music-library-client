ef352b684827554c8bb95ace70ceaf79d91f43d1
ItsAllDari <itsalldari@gmail.com>
2020-07-06 01:41:28 -0400

    Properly added the scripts for authentication
    Set up the events for sign up, sign in, sign out and changing password

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,63 @@
+'use strict'
+const config = require('./../config')
+const store = require('./../store')
+
+const newUser = function (formData) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data: {
+      credentials: {
+        email: formData.credentials.email,
+        password: formData.credentials.password,
+        password_confirmation: formData.credentials.password_confirmation
+      }
+    }
+  })
+}
+
+const userSignIn = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data: {
+      credentials: {
+        email: data.credentials.email,
+        password: data.credentials.password
+      }
+    }
+  })
+}
+
+const userSignOut = function () {
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+const changePassword = function (formData) {
+  return $.ajax({
+    method: 'PATCH',
+    url: config.apiUrl + '/change-password',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: {
+      passwords: {
+        old: formData.passwords.old,
+        new: formData.passwords.new
+      }
+    }
+  })
+}
+
+module.exports = {
+  newUser: newUser,
+  userSignIn: userSignIn,
+  userSignOut: userSignOut,
+  changePassword: changePassword
+}
---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,73 @@
+'use strict'
+const store = require('./../store')
+
+const newSuccess = function (data) {
+  $('form').trigger('reset')
+  $('#message').text('You have successfully created your account!')
+  $('#message').show().removeClass().addClass('success')
+}
+const newFailure = function () {
+  $('form').trigger('reset')
+  $('#message').text('Sign up failed!')
+  $('#message').show().removeClass().addClass('failure')
+}
+
+const signInSuccess = function (data) {
+  $('form').trigger('reset')
+  $('#message').text("You've signed in! Check out your song catalog below")
+  $('#message').show().removeClass().addClass('success')
+  store.user = data.user
+  $('.hide').show()
+  $('#sign-out').show()
+  $('.show').hide()
+  $('#getSongs').show()
+  $('#addSongs').show()
+}
+const signInFailure = function () {
+  $('form').trigger('reset')
+  $('#message').text('Sign in failed. Email or password is incorrect.')
+  $('#message').show().removeClass().addClass('failed')
+}
+
+const signOutSuccess = function () {
+  $('form').trigger('reset')
+  $('#message').text("You've signed out successfully! See you soon")
+  $('#message').show().removeClass().addClass('success')
+  store.user = null
+  $('.hide').hide()
+  $('.show').show()
+  $('.box').hide()
+  $('#getSongs').hide()
+  $('#updateSongs').hide()
+  $('#addSongs').hide()
+  $('.content').hide()
+  $('#clearSongs').hide()
+}
+const signOutFailure = function () {
+  $('form').trigger('reset')
+  $('#message').text('Sign out failed')
+  $('#message').show().removeClass().addClass('failed')
+}
+
+const changePasswordSuccess = function (data) {
+  $('form').trigger('reset')
+  $('#message').text('Your password has been updated')
+  $('#message').show().removeClass().addClass('success')
+}
+
+const changePasswordFailure = function () {
+  $('form').trigger('reset')
+  $('#message').text('Password change failed')
+  $('#message').show().removeClass().addClass('failed')
+}
+
+module.exports = {
+  newSuccess: newSuccess,
+  newFailure: newFailure,
+  signInSuccess: signInSuccess,
+  signInFailure: signInFailure,
+  signOutSuccess: signOutSuccess,
+  signOutFailure: signOutFailure,
+  changePasswordSuccess: changePasswordSuccess,
+  changePasswordFailure: changePasswordFailure
+}
