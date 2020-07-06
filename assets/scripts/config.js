da42502855ae3e0368723a37ce687d7c09e1f1ad
ItsAllDari <itsalldari@gmail.com>
2020-07-06 12:43:48 -0400

    Updated link for heroku

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://vast-atoll-95071.herokuapp.com/',
+  production: 'https://vast-atoll-95071.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
